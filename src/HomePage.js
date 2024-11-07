086d25e6e527cfc05b3ddecdcd85c12a100532f2
shruti <shruti.grover2001@gmail.com>
2024-11-08 02:04:14 +0530

    DAY-2

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,35 @@
 import React from "react";
 import "./HomePage.css";
-import Navbar from "./Navbar";
+import { useState } from "react";
+import Header from "./Header";
 
 const HomePage = () => {
+  const [formData, setFormData] = useState({ from: "", to: "", date: "" });
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can handle form data submission
+    console.log(formData);
+  };
+
   return (
     <div className="homepage">
-      <Navbar></Navbar>
+      <Header></Header>
       <h2>Book Your Train Ticket</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="from">From</label>
           <input
             type="text"
             id="from"
             placeholder="Enter Departure Station"
+            value={formData.from}
+            onChange={handleChange}
             required
           />
         </div>
@@ -23,12 +39,20 @@ const HomePage = () => {
             type="text"
             id="to"
             placeholder="Enter Arrival Station"
+            value={formData.to}
+            onChange={handleChange}
             required
           />
         </div>
         <div className="form-group">
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" required />
+          <input
+            type="date"
+            id="date"
+            value={formData.date}
+            onChange={handleChange}
+            required
+          />
         </div>
         <button type="submit">Search Trains</button>
       </form>
